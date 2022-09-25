3a0f3b8c7258e16269908cb5dad7710aae505ac2
Pavel Filippenko <rubikpf2002@mail.ru>
2022-09-25 12:20:13 +0300

    build own lib with functions and methods

---
+++ lab1/my_lib.py
@@ -0,0 +1,56 @@
+# The library with methods and functions to calcul and model sound and signals
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.integrate as integrate # импорт модуля численного интегрирования
+
+def quantize_uniform(x, quant_min=-1, quant_max=1, quant_level=5):
+    """Uniform quantization approach
+    From: Müller M. Fundamentals of music processing: Audio, analysis, algorithms, applications. – Springer, 2015.
+    Notebook: C2S2_DigitalSignalQuantization.ipynb
+    Args:
+        x: Original signal
+        quant_min: Minimum quantization level
+        quant_max: Maximum quantization level
+        quant_level: Number of quantization levels
+    Returns:
+        x_quant: Quantized signal
+    """
+    x_normalize = (x-quant_min) * (quant_level-1) / (quant_max-quant_min)
+    x_normalize[x_normalize > quant_level - 1] = quant_level - 1
+    x_normalize[x_normalize < 0] = 0
+    x_normalize_quant = np.around(x_normalize)
+    x_quant = (x_normalize_quant) * (quant_max-quant_min) / (quant_level-1) + quant_min
+    return x_quant
+
+def integrate_function(t, func, f, tau, real_part=True):
+    # Подынтегральное выражение для использованиия в функции integrate.quad
+    # t - время
+    # func - функция, задающая импульс
+    # f - частота
+    # tau - константа, используемая для задания длительности импульса
+    if real_part:
+        return func(t, tau)*np.cos(-2*np.pi*f*t)  # действительная часть интеграла
+    else:
+        return func(t, tau)*np.sin(-2*np.pi*f*t)  # мнимая часть интеграла
+
+def fourier_transform(signal, f_band, tau, t1, t2, res_type='abs'):
+    # вычисление преобразования Фурье для набора частот
+    # signal - функция от t и tau, задающая сигнал во временной области 
+    # f_band - набор частот, для которых вычисляется преобразование Фурье
+    # tau - константа, используемая для задания длительности импульса
+    # t1 момент начала сигнала
+    # t2 момент завершения сигнала
+    # тип возвращаемого значения:
+    # res_type='abs' - |X(f)|
+    # res_type='Re' - Re X(f)
+    # res_type='Im' - Im X(f)
+    if res_type=="abs":
+        Re=np.array([integrate.quad(integrate_function, t1, t2, args=(signal, f, tau, True))[0] for f in f_band])
+        Im=np.array([integrate.quad(integrate_function, t1, t2, args=(signal, f, tau, False))[0] for f in f_band])
+        return abs(Re+1j*Im)
+    elif res_type=="Re":
+        Re=np.array([integrate.quad(integrate_function, t1, t2, args=(signal, f, tau, True))[0] for f in f_band])
+        return Re
+    elif res_type=="Im":
+        Im=np.array([integrate.quad(integrate_function, t1, t2, args=(signal, f, tau, False))[0] for f in f_band])
+        return Im
